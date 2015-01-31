f89ae8c533eb92cb6ad3f01cfb9ae97d6388c35a
Zoraver <bigz97@outlook.com>
2015-01-31 14:01:15 -0500

    Added zeroThreshold.

--- src/org/usfirst/frc/team1517/utils/JoystickUtils.java
+++ src/org/usfirst/frc/team1517/utils/JoystickUtils.java
@@ -2,8 +2,11 @@ package org.usfirst.frc.team1517.utils;
 
 public class JoystickUtils {
 
+	static final double zeroThreshold = 0.1;
+	
 	public static double scaledStick(double input)
 	{
-		return Math.pow(input, 3);
+		if(Math.abs(input) < zeroThreshold) return 0;
+		else return Math.pow(input, 3);
 	}
 }
